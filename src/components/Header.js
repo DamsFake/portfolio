e05aea3a2497d15883298f1b239d8bac82643d7a
Damián Benítez Orozco <l20530179@cancun.tecnm.mx>
2025-02-27 20:57:09 -0500

    Añadido de CV en el Header y mejora de responsividad

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './Header.css';
 import FloatingShapes from './FloatingShapes';
+import { IoIosDownload  } from "react-icons/io";
 
 const Header = () => {
   return (
@@ -11,8 +12,12 @@ const Header = () => {
         <h2>Damián Benítez Orozco</h2>
         <h3>Ing. en Sistemas Computacionales</h3>
         <p>Especializado en crear experiencias web interactivas, responsivas y accesibles utilizando las tecnologías más modernas.</p>
+        <div className="buttonContainer">
         <a href="#proyectos" className="btn">Ver Proyectos</a>
-      </div>
+        <a href="https://drive.google.com/file/d/1CBZkBdbSXOYwY7-DcD_1z_O1n_3FzB8Z/view?usp=sharing" className="btn"> <IoIosDownload  size={20}/> Descargar CV</a>
+        </div>
+
+        </div>
     </header>
   );
 };
